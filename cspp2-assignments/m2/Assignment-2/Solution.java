abf8f1f03cdacef863a4142e7f3ebea652b1d554
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-28 20:31:52 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -8,7 +8,7 @@
 */
 import java.util.Scanner;
 /**
- * { Importing Math class }
+ * { Importing Math class }.
  */
 import java.lang.Math;
 /**This is Solution.*/
@@ -22,26 +22,26 @@ public final class Solution {
     Main .*/
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
     }
     /**
-     * { Compute roots for Quadratic equation }
+     * { Compute roots for Quadratic equation }.
      *
      * @param      a     { an input }
      * @param      b     { an input }
      * @param      c     { an input }
      */
     public static void rootsOfQuadraticEquation(final int a,
-    	final int b, final int c) {
-    	final int num = 4;
-    	final int num1 = 2;
-    	double result1 = (- b + Math.sqrt((b * b) - num * a * c))
-    		/(num1 * a);
-    	double result2 = (- b - Math.sqrt((b * b) - num * a * c))
-    		/(num1 * a);
-    	System.out.println(result1 + " " + result2);
+        final int b, final int c) {
+        final int num = 4;
+        final int num1 = 2;
+        double result1 = (-b + Math.sqrt((b * b) - num * a * c))
+            /(num1 * a);
+        double result2 = (-b - Math.sqrt((b * b) - num * a * c))
+            /(num1 * a);
+        System.out.println(result1 + " " + result2);
     }
 }
