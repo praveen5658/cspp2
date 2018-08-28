c8cdf5122eb85b2a3de7b8728b32976b3c670e27
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-28 20:46:38 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -0,0 +1,42 @@
+/**
+    * Autor : Praveen
+    * Date : 28-08-2018
+    * Calculate power
+*/
+/**
+    * Importing Scanner Class.
+*/
+import java.util.Scanner;
+/**This is Solution.*/
+public final class Solution {
+/**Calling Constructor.*/
+    private Solution() {
+        super();
+    }
+    /**
+    @param args
+    Main .*/
+    public static void main(final String[] args) {
+        Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result=power(base,exponent);
+        System.out.println(result);
+    }
+    /**
+     * { Calculate power }.
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { power value as double }
+     */
+    public static long power(final int base, final int exponent) {
+    	final int num = 1;
+    	if (exponent == 0) {
+    		return num;
+    	} else {
+    		return base * power(base, exponent - 1);
+    	}
+    }
+}
