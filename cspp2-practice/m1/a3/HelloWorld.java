21a351275e1024680df61ff789583e05332fa983
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-28 09:17:15 +0530

    m1.a3.HelloWorld

---
+++ cspp2-practice/m1/a3/HelloWorld.java
@@ -0,0 +1,13 @@
+/**This is HelloWorld Class.*/
+public final class HelloWorld {
+/**Main Method.*/
+    private HelloWorld() {
+        super();
+    }
+    /**
+    @param args
+    Main .*/
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
