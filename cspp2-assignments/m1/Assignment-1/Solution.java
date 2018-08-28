bbb6e5da396f7e3d392b0634bb24ba63257d9e14
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-28 11:46:54 +0530

    m1.assign1.NAme&Roll

---
+++ cspp2-assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,17 @@
+/**
+    * Autor : Praveen
+    * Date : 28-08-2018
+    * Printing Name and Roll NUmber
+*/
+public final class Solution {
+/**Main Method.*/
+    private Solution() {
+        super();
+    }
+    /**
+    @param args
+    Main .*/
+    public static void main(final String[] args) {
+        System.out.println("Name : " + args[0] +'\n'+"Roll No : " + args[1]);
+    }
+}
