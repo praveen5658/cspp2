6391bc24bc485816ca2e781e3749309b5d246012
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-30 21:54:55 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -9,30 +9,30 @@
 import java.util.Scanner;
 /**This is Solution class.*/
 public final class Solution {
-	/**Reverse of string*/
-	private Solution() {
-		super();
-	}
-	/**
-	@param args
-	Main .*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);
-		System.out.println(reverse);
-	}
-	/**
-	 * { reverse string }.
-	 *
-	 * @param      s     { string }.
-	 *
-	 * @return     { string }.
-	 */
-	public static String reverseString(final String s) {
-		StringBuilder str = new StringBuilder();
-		str.append(s);
-		str = str.reverse();
-		return str.toString();
-	}
+    /**Reverse of string.*/
+    private Solution() {
+        super();
+    }
+    /**
+    @param args
+    Main .*/
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+     * { reverse string }.
+     *
+     * @param      s     { string }.
+     *
+     * @return     { string }.
+     */
+    public static String reverseString(final String s) {
+        StringBuilder str = new StringBuilder();
+        str.append(s);
+        str = str.reverse();
+        return str.toString();
+    }
 }
