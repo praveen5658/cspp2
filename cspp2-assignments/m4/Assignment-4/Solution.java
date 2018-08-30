0c5b3aae5f6e4550612747d10a41b83b68158436
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-30 21:53:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -0,0 +1,38 @@
+/**
+    * Autor : Praveen
+    * Date : 30-08-2018
+    * Array Reverse of String
+*/
+/**
+    * Importing Scanner Class.
+*/
+import java.util.Scanner;
+/**This is Solution class.*/
+public final class Solution {
+	/**Reverse of string*/
+	private Solution() {
+		super();
+	}
+	/**
+	@param args
+	Main .*/
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String reverse = reverseString(s);
+		System.out.println(reverse);
+	}
+	/**
+	 * { reverse string }.
+	 *
+	 * @param      s     { string }.
+	 *
+	 * @return     { string }.
+	 */
+	public static String reverseString(final String s) {
+		StringBuilder str = new StringBuilder();
+		str.append(s);
+		str = str.reverse();
+		return str.toString();
+	}
+}
