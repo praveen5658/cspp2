b83e317bd069d802df4b5e0b416f5694456e6641
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-04 14:06:04 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m8/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/List.java
@@ -330,7 +330,6 @@ public class List {
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
             default:
-                System.out.println("Default");
                 break;
             }
         }
