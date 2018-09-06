acb1047cf711bc99b82777d13a40b29aac526d07
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-06 12:28:38 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+final class Solution {
     /**
      * Constructs the object.
      */
@@ -18,7 +18,7 @@ public class Solution {
     public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl;
+        StringListInterface l = (StringListInterface) sl;
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -62,6 +62,8 @@ public class Solution {
                 break;
             case "contains":
                 System.out.println(l.contains(tokens[1]));
+                break;
+            default :
                 break;
             }
         }
