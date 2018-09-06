9433fd714f55a03ebd02d05204ef341312f810bf
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-06 18:00:20 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -2,7 +2,7 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 /**
- * List of .
+ * List of Class.
  */
 public class List {
     //Implement all the methods mentioned to build a ListADT
