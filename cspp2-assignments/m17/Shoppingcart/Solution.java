09d7c93e6e6986eb382f00decdfacdd53384b8cc
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-14 18:11:31 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -0,0 +1,205 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class item {
+	private String itemName;
+	public int itemQuantity;
+	public float itemUnitPrice;
+	item(String name, int quantity, float price) {
+		itemName = name;
+		itemQuantity = quantity;
+		itemUnitPrice = price;
+	}
+	public String getitemName() {
+		return itemName;
+	}
+	public int getitemQuantity() {
+		return itemQuantity;
+	}
+	public float getitemUnitPrice() {
+		return itemUnitPrice;
+	}
+	public String toString() {
+		String s = "";
+		if (itemUnitPrice != 0.0) {
+			s = itemName + " " + String.valueOf(
+			               itemQuantity) + " " + String.valueOf(itemUnitPrice);
+		} else {
+			s = itemName + " " + String.valueOf(
+			               itemQuantity);
+		}
+		return s;
+	}
+}
+
+
+class ShoppingCart {
+	private item[] catalog;
+	private item[] cart;
+	private int catalogsize;
+	private int cartsize;
+	private int coupon;
+	ShoppingCart() {
+		catalog = new item[10];
+		cart = new item[10];
+		catalogsize = 0;
+		cartsize = 0;
+		coupon = 0;
+	}
+	public void resize() {
+		catalog = Arrays.copyOf(catalog, catalogsize +1);
+		cart = Arrays.copyOf(cart, cartsize +1);
+	}
+	public void addtoCatalog(item item1) {
+		if (catalogsize == catalog.length) {
+			resize();
+		}
+		catalog[catalogsize++] = item1;
+	}
+	public void showCatalog() {
+		for (int i = 0; i < catalogsize; i++) {
+			System.out.println(catalog[i]);
+		}
+	}
+	public void addtoCart(item item1) {
+		int k = 0;
+		if (cartsize == cart.length) {
+			resize();
+		}
+		for (int i = 0; i < catalogsize; i++) {
+			if (catalog[i].getitemName().equals(item1.getitemName()) &&
+			        (catalog[i].itemQuantity > item1.itemQuantity)) {
+				cart[cartsize++] = item1;
+				catalog[i].itemQuantity -= item1.itemQuantity;
+				break;
+			} else if (catalog[i].getitemName().equals(item1.getitemName()) &&
+			           (catalog[i].itemQuantity == item1.itemQuantity)) {
+				cart[cartsize++] = item1;
+				removefromCatalog(catalog[i]);
+				break;
+			} else {
+				k++;
+			}
+		}
+		if (k != 0) {
+			System.out.println("Item not existed");
+		}
+	}
+	public void showCart() {
+		for (int i = 0; i < cartsize; i++) {
+			System.out.println(cart[i]);
+		}
+	}
+	public float getTotalAmount() {
+		float f = 0.0f;
+		for (int i = 0; i<cartsize;i++) {
+			for (int j = 0;j< catalogsize;j++) {
+				if (cart[i].getitemName().equals(catalog[j].getitemName())) {
+					f += cart[i].itemQuantity * catalog[j].itemUnitPrice;
+					break;
+				}
+			}
+		}
+		return f;
+	}
+	public void removefromCart(item item1) {
+		item[] sample = new item[cartsize-1];
+		for (int i = 0,j=0; i<cartsize-1;i++) {
+			if (!(cart[i].equals(item1))) {
+				sample[j] = cart[i];
+				j++;
+			}
+		}
+		cart = sample.clone();
+		cartsize -= 1;
+		for (int k=0;k<catalogsize;k++) {
+			if (catalog[k].getitemName().equals(item1.getitemName())) {
+				catalog[k].itemQuantity += item1.itemQuantity;
+			}
+		}
+	}
+	public void removefromCatalog(item item1) {
+		item[] sample = new item[catalogsize-1];
+		for (int i = 0,j=0; i<catalogsize;i++) {
+			if (!(catalog[i].equals(item1))) {
+				sample[j] = catalog[i];
+				j++;
+			}
+		}
+		catalog = sample.clone();
+		catalogsize -= 1;
+	}
+	public void applyCoupon(final int disc) {
+		if (coupon < disc) {
+			coupon = disc;
+		}
+	}
+	public float getpaybleAmount() {
+		if (coupon == 0) {
+			return (getTotalAmount() * 1.15f);
+		}
+		return ((getTotalAmount()/coupon) * 1.15f);
+	}
+	public void printInvoice() {
+		System.out.println("Name"+"   "+"Quantity"+"   "+"Price");
+		showCart();
+		System.out.println("Total:" +getTotalAmount());
+		if (coupon == 0) {
+			System.out.println("Disc%:0.0");
+		} else {
+			System.out.println("Disc%:"+(getTotalAmount()/coupon));
+		}
+		System.out.println("Tax:"+(getTotalAmount() * 0.15f));
+		System.out.println("Payble amount:" +getpaybleAmount());
+	}
+}
+
+
+public final class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testcases = Integer.parseInt(scan.nextLine());
+		ShoppingCart sc = new ShoppingCart();
+		for (int i = 0; i < testcases; i++) {
+			String[] token = (scan.nextLine()).split(" ");
+			switch (token[0]) {
+			case "Item":
+				String[] values = token[1].split(",");
+				int quantity = Integer.parseInt(values[1]);
+				float price = Float.parseFloat(values[2]);
+				sc.addtoCatalog(new item(values[0], quantity, price));
+				break;
+			case "catalog":
+				sc.showCatalog();
+				break;
+			case "add":
+				values = token[1].split(",");
+				quantity = Integer.parseInt(values[1]);
+				sc.addtoCart(new item(values[0], quantity, 0.0f));
+				break;
+			case "show":
+				sc.showCart();
+				break;
+			case "totalAmount":
+				System.out.println("totalAmount:" + sc.getTotalAmount());
+				break;
+			case "remove":
+				values = token[1].split(",");
+				quantity = Integer.parseInt(values[1]);
+				sc.removefromCart(new item(values[0], quantity, 0.0f));
+				break;
+			case "coupon":
+				int disc = Integer.parseInt(token[1].substring(3,5));
+				if (disc == 10 || disc == 20 || disc == 30 ||disc == 50) {
+					sc.applyCoupon(disc);
+				} else {
+					System.out.println("Not a valid coupon");
+				}
+				break;
+			case "paybleAmount":
+				System.out.println("Payble amount:" + sc.getpaybleAmount());
+			case "print":
+				sc.printInvoice();
+			}
+		}
+	}
+}
