afe8dea8a3f7a5715a38612584ebdd726d8abf0c
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-15 12:14:22 +0530

    "check_style_score -> 4 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -117,7 +117,7 @@ class Quiz {
         int penalityMarks;
         for (int i = 0; i < questionCount; i++) {
             tokens = s.nextLine().split(":");
-            if (tokens.length == 5) {
+            if (tokens.length == 5 && tokens[0] != null) {
                 choice = tokens[1].split(",");
                 correctAnswer = Integer.parseInt(tokens[2]);
                 if (choice.length >= 2) {
