6e04c6cf87a2c5edd6611a80d978eb36a5914425
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-20 18:11:42 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -16,10 +16,11 @@ public class Solution {
 				long maxval = 0;
 				String file1 = null;
 				String file2 = null;
+				System.out.print("\t\t");
 				for (int i = 0; i < files.length - 1; i++) {
 					System.out.print(files[i].getName() + "\t");
 				}
-				System.out.println(files[files.length - 1].getName());
+				System.out.print(files[files.length - 1].getName());
 				System.out.println();
 				for (File firstfile : files) {
 					System.out.print(firstfile.getName() + "\t");
