cbd5899aef2c415ed47b1b7ccfdc3595e2fa61a3
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-22 12:05:35 +0530

    "check_style_score -> 13 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -256,7 +256,7 @@ class Todoist {
 	}
 	public int totalTime4Completion() {
 		int sum = 0;
-		for (int i = 0; i< tasks.length;i++) {
+		for (int i = 0; i< size;i++) {
 			if (tasks[i].getStatus().equals("todo")) {
 				sum += tasks[i].getTime();
 			}
