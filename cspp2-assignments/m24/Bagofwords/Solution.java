6695e7423f55ce2410b5d0415b46743adbe336f4
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-23 17:05:39 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

---
+++ cspp2-assignments/m24/Bagofwords/Solution.java
@@ -0,0 +1,70 @@
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+import java.io.File;
+/**
+ * Class for hello solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+            File dir = new File(input);
+            File[] files = dir.listFiles();
+            final int hundred = 100;
+            if (files.length == 0) {
+                System.out.println("empty directory");
+            } else {
+                Distance map1;
+                Distance map2;
+                double ans;
+                long maxval = 0;
+                String file1 = null;
+                String file2 = null;
+                System.out.print("\t\t");
+                for (int i = 0; i < files.length - 1; i++) {
+                    System.out.print(files[i].getName() + "\t");
+                }
+                System.out.print(files[files.length - 1].getName());
+                System.out.println();
+                for (File firstfile : files) {
+                    System.out.print(firstfile.getName() + "\t");
+                    map1 = new Distance(firstfile);
+                    for (File secondfile : files) {
+                        map2 = new Distance(secondfile);
+                        ans = map1.angle(map2.getMap());
+                        if (Math.round(ans * hundred) > maxval
+                                && !firstfile.getName().equals(
+                                    secondfile.getName())) {
+                            maxval = Math.round(ans * hundred);
+                            file1 = firstfile.getName();
+                            file2 = secondfile.getName();
+                        }
+                        System.out.print(Math.round(ans * hundred) + "\t\t");
+                    }
+                    System.out.print("\n");
+                }
+                System.out.println("Maximum similarity is between "
+                                   + file1 + " and " + file2);
+            }
+        } catch (Exception e) {
+            System.out.println("empty directory");
+        }
+    }
+}
